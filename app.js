cdbc9e1ee819c7d09dfdea340ed900b403703daa
Osmani Hernandez <osmanihernandezperez@gmail.com>
2026-04-19 03:50:23 -0400

    configure JWT auth and validation

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const { ApolloServer } = require("apollo-server-express")
+const { ApolloServer } = require("apollo-server-express");
 const {
   ApolloServerPluginLandingPageGraphQLPlayground,
   ApolloServerPluginDrainHttpServer,
-} = require("apollo-server-core")
-const { execute, subscribe } = require("graphql")
-const { makeExecutableSchema } = require("@graphql-tools/schema")
-const { SubscriptionServer } = require("subscriptions-transport-ws")
-const http = require("http")
-const express = require("express")
-const jwt = require("jsonwebtoken")
-const schema = require("./graphql/schema/schema")
+} = require("apollo-server-core");
+const { execute, subscribe } = require("graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const http = require("http");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const schema = require("./graphql/schema/schema");
 
-const User = require("./models/user")
+const User = require("./models/user");
 
 module.exports = async function startApolloServer() {
-  const app = express()
-  const httpServer = http.createServer(app)
-  
+  const app = express();
+  const httpServer = http.createServer(app);
+
   const server = new ApolloServer({
     schema,
     context: async ({ req }) => {
-      const auth = req ? req.headers.authorization : null
+      const auth = req ? req.headers.authorization : null;
 
       if (auth && auth.toLowerCase().startsWith("bearer ")) {
         const decodedToken = jwt.verify(
           auth.substring(7),
-          process.env.JWT_SECRET
-        )
-        const currentUser = await User.findById(decodedToken.id)
-        return { currentUser }
+          process.env.JWT_SECRET,
+        );
+        const currentUser = await User.findById(decodedToken.id);
+        return { currentUser };
       }
     },
     plugins: [
@@ -38,13 +38,13 @@ module.exports = async function startApolloServer() {
         async serverWillStart() {
           return {
             async drainServer() {
-              subscriptionServer.close()
+              subscriptionServer.close();
             },
-          }
+          };
         },
       },
     ],
-  })
+  });
 
   const subscriptionServer = SubscriptionServer.create(
     {
@@ -55,17 +55,17 @@ module.exports = async function startApolloServer() {
     {
       server: httpServer,
       path: "/graphql",
-    }
-  )
+    },
+  );
 
-  await server.start()
+  await server.start();
   server.applyMiddleware({
     app,
-    path: "/",
-  })
+    path: "/graphql",
+  });
 
   await new Promise((resolve) =>
-    httpServer.listen({ port: process.env.PORT }, resolve)
-  )
-  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
-}
+    httpServer.listen({ port: process.env.PORT }, resolve),
+  );
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+};
